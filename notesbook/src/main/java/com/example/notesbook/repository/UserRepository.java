a28ab127249d273f5958194ae5c3a2d08e5e58bf
wali <wk0697143@gmail.com>
2026-03-27 21:35:35 +0500

    Add UserRepository with findByEmail method

---
+++ notesbook/src/main/java/com/example/notesbook/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.example.notesbook.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.notesbook.entity.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
