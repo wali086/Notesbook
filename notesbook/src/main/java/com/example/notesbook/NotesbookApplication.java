5153fe3c42962d7a77be5b6ae2ac0367bc5f9555
wali <wk0697143@gmail.com>
2026-03-27 21:31:30 +0500

    Add NotesbookApplication main class

---
+++ notesbook/src/main/java/com/example/notesbook/NotesbookApplication.java
@@ -0,0 +1,13 @@
+package com.example.notesbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotesbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotesbookApplication.class, args);
+	}
+
+}
