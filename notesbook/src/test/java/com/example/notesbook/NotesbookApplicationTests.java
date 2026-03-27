11426c91c608a638238c5b861b5d2c5c9e8f5ece
wali <wk0697143@gmail.com>
2026-03-27 21:46:19 +0500

    Add NotesbookApplicationTests

---
+++ notesbook/src/test/java/com/example/notesbook/NotesbookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.notesbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotesbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
